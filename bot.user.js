491dbddff4c6a6037eec4087cd87f235adc02a00
Px <19843204+pxnt@users.noreply.github.com>
2020-04-03 09:23:32 +0200

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,27 +1,21 @@
 // ==UserScript==
 // @name         Reddit April Fools Imposter Bot
 // @namespace    jrwr.io
-// @version      1.0.10
+// @version      1.1.7
 // @description  A bot that randomly chooses a entry and reports back to a central database at spacescience.tech
-// @author       dimden updated by jrwr
+// @author       dimden (https://dimden.dev/), jrwr (http://jrwr.io/), px, qqii, cg
 // @match        https://gremlins-api.reddit.com/room?nightmode=1&platform=desktop
-// @match        https://gremlins-api.reddit.com/room?nightmode=1&platform=desktop*
+// @match        https://gremlins-api.reddit.com/room*
 // @match        https://gremlins-api.reddit.com/results?*
-
+// @require      https://github.com/LeoVerto/doorman/raw/master/doorman-lib.js?v=0.9
+// @updateurl    https://github.com/jrwr/imposterbot/raw/master/bot.user.js
 // ==/UserScript==
-
-const DETECTOR_URL = "https://detector.abra.me/?";
-const CHECK_URL = "https://librarian.abra.me/check";
-const SUBMIT_URL = "https://librarian.abra.me/submit";
-const SPACESCIENCE_URL = "https://spacescience.tech/check.php?id=";
-const OCEAN_URL = "https://wave.ocean.rip/answers/answer?text=";
-var locked = 0;
 
 document.getElementsByTagName("head")[0].insertAdjacentHTML(
     "beforeend",
     "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css\" />");
 
-var imported = document.createElement('script');
+let imported = document.createElement('script');
 imported.src = 'https://cdn.jsdelivr.net/npm/toastify-js';
 document.head.appendChild(imported);
 
@@ -37,28 +31,6 @@ async function getRoom() {
     };
 };
 
-async function checkExistingSpacescience(id) {
-
-   let res = await (await fetch("https://spacescience.tech/check.php?id=" + id)).text();
-   let json = JSON.parse(res);
-
-
-    console.log(json);
-
-    for (key in json) {
-        if (json[key].hasOwnProperty("flag")) {
-            if (json[key].flag = 1) {
-                console.log(json[key]);
-                switch(json[key].result) {
-                    case "LOSE":
-                        return "human";
-                }
-            }
-        }
-    }
-    return "bot";
-}
-
 
 async function submitAnswer(token, id) {
     let body = new FormData();
@@ -73,29 +45,46 @@ async function submitAnswer(token, id) {
     return JSON.parse(res);
 }
 async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
 }
+
+
 async function play() {
     let room = await getRoom();
-    let answer = 0, found = false;
-    console.log(room.options);
+    let answer = 0,
+        maxDetector = 0;
+
+    let abraP = checkExistingAbra(room.options.flatMap(x => x[0]));
+    let spacP = Promise.all(room.options.flatMap(x => checkExistingSpacescience(x[0])));
+    // let deteP = Promise.all(room.options.flatMap(x => checkDetector(x[1])));
+    // cost of accuracy
+
+    let [abra, space/*, detector*/] = await Promise.all([abraP, spacP/*, deteP*/]);
+
+    // console.table(abra);
+    // console.table(space);
+    // console.table(detector);
 
     for (let i = 0; i < room.options.length; i++) {
-        [o, z] = room.options[i];
-        let space = await checkExistingSpacescience(o);
-        console.log(o);
-        if(space === "human"){
-           
-        }else{
+        // o is id
+        // z is string
+        let [o, z] = room.options[i];
+        if (abra[i] === "known fake" || space[i] === "known fake") {
             answer = i;
-            console.log("Picking "+z+" with " + o[1]);
             break;
+        } else if (abra[i] === "known human" || space[i] === "known human") {
+            continue;
+        } else if (abra[i] === "unknown" && space[i] === "unknown") {
+            answer = i;
+            // if (detector[i] > maxDetector) {
+            //     maxDetector = detector[i];
+            //     answer = i;
+            //     continue;
+            // }
         }
-
     };
-
 
     let result = await submitAnswer(room.token, room.options[answer][0]);
 
@@ -115,46 +104,92 @@ async function submitAnswerToDB(answer, result, room) {
 
     return JSON.parse(res);
 }
+
+let timing = [];
+
 function getStats() {
-    console.log(wins);
-    return `All: ${wins.length+loses.length}
-Wins: ${wins.length} (${((wins.length/(wins.length+loses.length))*100).toFixed(1)}%)
-Loses: ${loses.length} (${((loses.length/(wins.length+loses.length))*100).toFixed(1)}%)
+    const sum = timing.reduce((a, b) => a + b, 0);
+    const avg = (sum / timing.length) || 0;
+
+//     console.log(wins);
+    return `All: ${wins.length+loses.length} -
+Wins: ${wins.length} (${((wins.length/(wins.length+loses.length))*100).toFixed(1)}%),
+Loses: ${loses.length} (${((loses.length/(wins.length+loses.length))*100).toFixed(1)}%), Time (ms): ${avg}
 `;
 }
 
+window.last = "INVALID";
 window.wins = []; window.loses = [];
 setInterval(async () => {
-	if(locked === 0){
-	locked = 1
+    let t0 = performance.now();
     let game = await play();
-    let submit = await submitAnswerToDB(game[0].trim(), game[1], game[2]);
-    game[0] = game[0].trim();
-    if(game[1] === "WIN") wins.push(game[0]);
-    else if(game[1] === "LOSE") loses.push(game[0]);
-Toastify({
-  text: game[0] + " "+ game[1],
-  duration: 1000, 
-  newWindow: true,
-  close: true,
-  gravity: "top", // `top` or `bottom`
-  position: 'left', // `left`, `center` or `right`
-  backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
-  stopOnFocus: false, // Prevents dismissing of toast on hover
-}).showToast();
-   locked = 0;
-}
-}, 2000)
+    submitAnswerToDB(game[0].trim(), game[1], game[2]).then(
+        function (submit) {
+            let t1 = performance.now();
+            timing.push(t1 - t0);
+            game[0] = game[0].trim();
+            if(game[1] === "WIN") wins.push(game[0]);
+            else if(game[1] === "LOSE") loses.push(game[0]);
+            last = game[1];
+            if (game[1] == "WIN")
+            {
+                Toastify({
+                  text: game[1] + ": "+ game[0],
+                  duration: 5000,
+                  newWindow: true,
+                  close: true,
+                  gravity: "top", // `top` or `bottom`
+                  position: 'left', // `left`, `center` or `right`
+                  backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+                  stopOnFocus: false, // Prevents dismissing of toast on hover
+                }).showToast();
+            }
+            else if (game[1] == "LOSE")
+            {
+                Toastify({
+                  text: game[1] + ": "+ game[0],
+                  duration: 5000,
+                  newWindow: true,
+                  close: true,
+                  gravity: "top", // `top` or `bottom`
+                  position: 'left', // `left`, `center` or `right`
+                  backgroundColor: "linear-gradient(to right, #b00023, #c93d54)",
+                  stopOnFocus: false, // Prevents dismissing of toast on hover
+                }).showToast();
+            }
+            else if (game[1] == "INVALID")
+            {
+                Toastify({
+                  text: game[1] + ": "+ game[0],
+                  duration: 5000,
+                  newWindow: true,
+                  close: true,
+                  gravity: "top", // `top` or `bottom`
+                  position: 'left', // `left`, `center` or `right`
+                  backgroundColor: "linear-gradient(to right, #423e3f, #8f8b8c)",
+                  stopOnFocus: false, // Prevents dismissing of toast on hover
+                }).showToast();
+            }
+        }
+    )
+}, 1200)
+
 setInterval(() => {
     let curstatus = getStats();
 Toastify({
   text: curstatus,
-  duration: 10000, 
+  duration: 10000,
   newWindow: true,
-  close: true,
-  gravity: "top", // `top` or `bottom`
-  position: 'left', // `left`, `center` or `right`
-  backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+  close: false,
+  gravity: "bottom", // `top` or `bottom`
+  position: 'right', // `left`, `center` or `right`
+  backgroundColor: "linear-gradient(to right, #cf00c1, #3d49c9)",
   stopOnFocus: false, // Prevents dismissing of toast on hover
 }).showToast();
-}, 20000);
+
+    if(location.href.includes("results?") && !document.hidden) fetch(`https://gremlins-api.reddit.com/results?prev_result=${last}&nightmode=1&platform=desktop`).then(i => i.text()).then(html => {
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(html, "text/html");
+        document.getElementsByTagName("gremlin-app")[0].innerHTML = doc.getElementsByTagName("gremlin-app")[0].innerHTML;
+    })
+}, 10000);
